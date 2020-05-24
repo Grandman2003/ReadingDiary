8bc5b1d0a1c578934015294f131f1b459ee04e1b
SuperCoon <56171741+SuperCoon666@users.noreply.github.com>
2020-05-24 18:20:36 +0300

    update

--- app/src/main/java/com/example/readingdiary/Activities/OnlineActivity.java
+++ app/src/main/java/com/example/readingdiary/Activities/OnlineActivity.java
@@ -20,6 +20,7 @@ public class OnlineActivity extends AppCompatActivity
     Button bShareUser;
     Button bGoLent;
     EditText etShareUser;
+    String [] x = new String[] {"wsdsaddaads","adsdsaa ","dsadsdsad " };// тестовый массив подписок
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -40,9 +41,10 @@ public class OnlineActivity extends AppCompatActivity
             {
 //                Intent intent = new Intent(OnlineActivity.this, OnlineActivity.class);
 //                startActivity(intent);
+                x[x.length]=etShareUser.getText().toString();
                 ShareUser(etShareUser.getText().toString());
                 // добавление usID к списку подписок
-                Toast.makeText(OnlineActivity.this,"Пользователь добавлен в подписки",Toast.LENGTH_SHORT).show();
+                Toast.makeText(OnlineActivity.this,"Пользователь добавлен в подписки ",Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -63,10 +65,10 @@ public class OnlineActivity extends AppCompatActivity
     {
 
         //обращение к списку id, на которые подписан usID
-        String[] ids = new String[] {""," "," " }; // массив подписок
+        //String[] ids = new String[] {""," "," " }; // массив подписок
         ArrayList <String> gotcha = new ArrayList<>(); // список совпадений
         QuickSort Share = new QuickSort(); // класс сортировки
-        Share.StrSort(ids,usID,gotcha); // сортировка и поиск совпадений
+        Share.StrSort(x,usID,gotcha); // сортировка и поиск совпадений
         usID = String.valueOf(gotcha); // поскльку совпадение может быть только одно записываем его в str для добавления
 
 
